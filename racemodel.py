5e695504b010c1d170f55ca820a9581930ded746
Andrew Chew <andrew@okashistudios.com>
2018-10-12 23:24:03 -0700

    Remove some deprecated code in racemodel
    No need to have code to calculate elapsed time in the racemodel.

--- racemodel.py
+++ racemodel.py
@@ -22,18 +22,6 @@ class Field(BaseModel):
     # JSON for miscellaneous data (has no meaning for our model)..
     data = TextField()
 
-# A racer in a time trial. Has a start and finish time. The result is redefined
-# to be a timedelta, and setting of start, finish, and result are maintained to
-# be consistent.
-#
-# To set a racer's start time to today at 9:30am:
-#   racer.start = datetime.datetime.now().replace(hours=9, minutes=30, seconds=0, microseconds=0)
-#
-# To set a racer's finish time to right now:
-#   racer.finish = datetime.datetime.now()
-#
-# To set a racer's result time to 19 minutes:
-#   racer.result = datetime.timedelta(minutes=19)
 class Racer(BaseModel):
     bib = IntegerField(unique=True)
     name = CharField()
@@ -46,17 +34,6 @@ class Racer(BaseModel):
 
     # JSON for miscellaneous data (has no meaning for our model)..
     data = TextField()
-
-    @property
-    def result(self):
-        if not self.finish or not self.start:
-            return None
-
-        return self.finish - self.start
-
-    @result.setter
-    def result(self, result):
-        self.finish = self.start + result
 
 class Result(BaseModel):
     finish = TimeField()
