d4c9b0c4531b31a9c22a34c25b4cbfe1fc8ef79f
Andrew Chew <andrew@okashistudios.com>
2018-10-23 18:37:13 -0700

    Added pretty_list function
    This function takes a list of strings, and makes a nice, comma-separated
    list (with oxford comma), using op as the conjunction.

--- common.py
+++ common.py
@@ -1,2 +1,12 @@
 APPLICATION_NAME = 'SexyThyme'
 VERSION = '1.0'
+
+def pretty_list(lst, op='and'):
+    if len(lst) == 0:
+        return ''
+    if len(lst) == 1:
+        return lst[0]
+    if len(lst) == 2:
+        return lst[0] + ' ' + op + ' ' + lst[1]
+
+    return ', '.join(lst[0:-1]) + ', ' + op + ' ' + lst[-1]
