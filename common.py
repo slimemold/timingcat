76ed81d412196ffd847e4f9360a78d3bd1973b23
Andrew Chew <andrew@okashistudios.com>
2018-10-24 00:40:36 -0700

    Added pluralize
    This function takes a word and a count, and returns None if count is 0,
    the word if the count is 1, and the word + 's' if the count is > 1.
    Used in conjunction with pretty_list.

--- common.py
+++ common.py
@@ -2,6 +2,9 @@ APPLICATION_NAME = 'SexyThyme'
 VERSION = '1.0'
 
 def pretty_list(lst, op='and'):
+    # Filter out any None's.
+    lst = list(filter(None.__ne__, lst))
+
     if len(lst) == 0:
         return ''
     if len(lst) == 1:
@@ -10,3 +13,12 @@ def pretty_list(lst, op='and'):
         return lst[0] + ' ' + op + ' ' + lst[1]
 
     return ', '.join(lst[0:-1]) + ', ' + op + ' ' + lst[-1]
+
+def pluralize(word, count):
+    if count == 0:
+        return None
+
+    if count == 1:
+        return '%s %s' % (count, word)
+
+    return '%s %s' % (count, word + 's')
