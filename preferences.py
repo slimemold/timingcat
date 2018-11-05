43207da8887abcdf3e55bf93e9a0bda14ecceb29
Andrew Chew <andrew@okashistudios.com>
2018-11-04 21:52:09 -0800

    Linted preferences.py

--- preferences.py
+++ preferences.py
@@ -1,6 +1,12 @@
-from PyQt5.QtWidgets import *
+"""Preferences Qt Classes
+
+This module implements a QDialog that can be used to set application preferences.
+"""
+
+from PyQt5.QtWidgets import QDialog
 
 class PreferencesWindow(QDialog):
+    """This dialog allows the user to set application preferences."""
     def __init__(self, modeldb, parent=None):
         super().__init__(parent=parent)
 
